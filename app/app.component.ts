6fd6722548d6c84569eb17a9483cad3a53a74328
husnu <husnu.tapan@gmail.com>
2016-06-13 01:32:11 +0300

    publish demo part

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,12 @@
 import {Component} from '@angular/core';
-
-
+import {InputmaskComponent} from "./component/inputmask/inputmaskComponent";
 @Component({
     selector: 'my-app',
     template:`
-       Hello World!!!
+        <my-mask></my-mask>
     `,
+    directives:[InputmaskComponent]
+
 })
 export class AppComponent {
 
