992c4f5fea9520173ccad58c6fc6ecf9d1e92653
husnu <husnu.tapan@gmail.com>
2016-06-12 22:06:20 +0300

    commit for not edited

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,11 @@
 import {Component} from '@angular/core';
-import {jQueryComponent} from "./component/choosen/jQueryComponent";
-import {InputmaskComponent} from "./component/inputmask/inputmaskComponent";
+
 
 @Component({
     selector: 'my-app',
     template:`
         <my-mask></my-mask>
     `,
-    directives:[InputmaskComponent]
-
 })
 export class AppComponent {
 
